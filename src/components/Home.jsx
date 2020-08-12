4c2c5e615505b52457a6d04b65d76252a9f4783d
Priyanka Rajput <025.priyanka@gmail.com>
2020-08-12 16:13:23 +0530

    create ToDo app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React ,{ useState }  from 'react';
+import  ToDoLists from '../components/ToDoLists';
+import '../assets/css/home.css';
 
 const Home = () => {
+
+  const [inputList, setInputList] = useState();
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+  });
+  setInputList("");
+};
+
+const deleteItems = (id) =>{
+  setItems((oldItems) => {
+    return oldItems.filter((arrElem, index) => {
+      return index !== id;
+    })
+  })
+};
+
   return (
-    <h1>welcome to home page</h1>
+    <>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>TODO List</h1>
+        <br />
+        <input type="text" placeholder="Add a items" value={inputList} onChange={itemEvent} />
+        <button onClick={listOfItems}> + </button>
+        <ol>
+          {/* <li>{inputList}</li> */}
+
+          { Items.map( (itemval, index) => {
+            return <ToDoLists 
+              key= {index} id={index} text= {itemval} onSelect= {deleteItems} />;
+          }) }
+
+        </ol>
+      </div>
+    </div>
+    </>
   );
 }
 
